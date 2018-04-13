665ad45f49c0c6b90927e2a1b6032665a43d678e
Max Moser <maxmanski@live.at>
2018-04-13 11:50:14 +0200

    Implement deserialization of index

--- src/search.py
+++ src/search.py
@@ -2,6 +2,8 @@
 
 import argparse
 from math import log10
+import os, os.path
+import pickle
 from pprint import pprint
 
 from util.topicParser import parse_topic
@@ -31,6 +33,9 @@ b          = args.b
 topic_file = args.topic_file
 DEBUG      = args.debug
 
+# TODO...
+query      = "I analyse but drink many ECTS"
+
 dbg("Activated Options")
 dbg("Scoring       : %s" % scoring)
 dbg("Parameter k_1 : %s" % k1)
@@ -40,7 +45,36 @@ dbg("Topic File    : %s" % topic_file)
 dbg()
 
 
-# TODO read index from file.
+if not os.path.isfile("index"):
+    print("No 'index' file could be found! Aborting.")
+    print("Please execute the indexer first")
+    exit(1)
+
+with open("index", "rb") as index_file:
+    idx = pickle.load(index_file)
+
+document_lengths = idx.document_lengths
+postings_list = idx.postings_list
+special = idx.special_strings
+case = idx.case_folding
+stop = idx.stop_words
+lemma = idx.lemmatization
+stem = idx.stemming
+
+dbg("Deserialized Index")
+dbg("Special : %s" % idx.special_strings)
+dbg("Case    : %s" % idx.case_folding)
+dbg("Stop    : %s" % idx.stop_words)
+dbg("Lemma   : %s" % idx.lemmatization)
+dbg("Stemming: %s" % idx.stemming)
+dbg("Docs Len: %s" % idx.document_lengths)
+dbg("PostList:")
+if DEBUG:
+    for key, x in postings_list.items():
+        dbg("%s" % x)
+        for d in document_lengths:
+            dbg("  > %dx in '%s'" % (x.occurrences_in(d), d))
+dbg("=" * 80)
 
 
 # TODO additional stopwords? ['document', 'relevant', 'mention'] => add to tokenizer options?
@@ -52,9 +86,11 @@ pprint(tokenized_topics)
 
 
 # TODO adapt code. calc scores for each document & topic, then print top 1000
+doc_lens = [l for d, l in document_lengths.items()]
 document_scores = {}
-avg_document_length = sum(document_lengths)/len(document_lengths)
+avg_document_length = sum(doc_lens) / len(doc_lens)
 for w in query.split():
+    # FIXME what if the word w does not occur in any of the postings?
     posting_item = postings_list[w]
     idf = log10(len(document_lengths) / posting_item.count())
     for d in posting_item.document_list:
