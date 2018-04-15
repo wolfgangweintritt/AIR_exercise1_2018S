ab8b5c22b31a8a3a817bb61bce84d2017665ae0d
Max <maxmanski@live.at>
2018-04-15 15:40:18 +0200

    Restructure indexing algorithm for drastically improved memory efficiency

--- src/indexer.py
+++ src/indexer.py
@@ -39,35 +39,6 @@ def expand_directories(files):
     return tmp
 
 
-def create_assoc(token, document):
-    """Create tuple that associates the token with a document"""
-    return (token, document)
-
-
-def sort_by_terms_and_doc(assoc_list):
-    """Sort the association list by token primarily and doc_id secondarily"""
-    # assumes to have form [(token, doc_id)]
-    assocs = assoc_list.copy()
-    assocs.sort(key=lambda x: x[1])
-    assocs.sort(key=lambda x: x[0])
-    return assocs
-
-
-def create_postings_list(assoc_list):
-    """Create a list of tokens associated with document frequency and document list"""
-    # assumes to have a sorted assoc_list
-    postings_list = {}
-    old_tkn = None
-    for (t, d) in assoc_list:
-        if old_tkn is None or old_tkn.token != t:
-            old_tkn = PostingsListItem(t, [d])
-            postings_list[old_tkn.token] = (old_tkn)
-        else:
-            old_tkn.add_doc(d)
-            
-    return postings_list
-
-
 # add argument parsing
 parser = argparse.ArgumentParser(usage="Creates an inverted index for documents",
                                  epilog="Maximilian Moser and Wolfgang Weintritt, 2018")
@@ -103,44 +74,57 @@ dbg("Stemming: %s" % stemming)
 dbg("Files   : %s" % files)
 dbg()
 
-# read each file and process their tokens
-tokenizer = Tokenizer(case, special, stop, stemming, lemma)
-assoc_list = []
-document_lengths = {}
-documents = []
-for i, f in enumerate(files):
-    docs = []
-    percent_done = ((i + 1) / len(files)) * 100
-    print("file %d/%d (%.2f%%)" % (i + 1, len(files), percent_done))
-    with open(f, encoding=encoding) as read_file:
-        # parse the documents from each file
-        content = read_file.read()
+try:
+    # read each file and process their tokens
+    postings_list = {}
+    tokenizer = Tokenizer(case, special, stop, stemming, lemma)
+    assoc_list = []
+    document_lengths = {}
+    no_files = len(files)
+
+    for i, f in enumerate(files):
+        docs = []
+        percent_done = ((i + 1) / no_files) * 100
+        content = ""
+        print("file %d/%d (%.2f%%)" % (i + 1, len(files), percent_done))
+
+        with open(f, encoding=encoding) as read_file:
+            # parse the documents from each file
+            content = read_file.read()
+            read_file.close()
+
         docs = document.parse_documents(content)
 
-    for doc in docs:
-        # tokenize each document
-        # and construct the association list (used for the postings list)
-        documents.append(doc)
-        tokens = tokenizer.tokenize(doc.text)
-        document_lengths[doc.id] = len(tokens)
-        for t in tokens:
-            assoc = create_assoc(t, doc.id)
-            assoc_list.append(assoc)
+        for doc in docs:
+            # tokenize each document
+            # and construct the association list (used for the postings list)
+            tokens = tokenizer.tokenize(doc.text)
+            document_lengths[doc.id] = len(tokens)
 
-# sort by token and document
-# and create 
-assoc_list = sort_by_terms_and_doc(assoc_list)
-postings_list = create_postings_list(assoc_list)
+            for t in tokens:
+                # increase the occurrences of the token in the document
+                if t in postings_list:
+                    postings_list[t].add_doc(doc.id)
+                else:
+                    postings_list[t] = PostingsListItem(t, [doc.id])
 
-if DEBUG:
-    for key, x in postings_list.items():
-        dbg("%s" % x)
-        for d in documents:
-            dbg("  > %dx in '%s'" % (x.occurrences_in(d.id), d.id))
+    # debug prints if specified
+    if DEBUG:
+        for key, x in postings_list.items():
+            dbg("%s" % x)
+            for d in document_lengths:
+                dbg("  > %dx in '%s'" % (x.occurrences_in(d), d))
 
-idx = Index(postings_list, document_lengths,
-            special, case, stop, lemma, stemming)
+    idx = Index(postings_list, document_lengths,
+                special, case, stop, lemma, stemming)
 
-# persist the Index object with the pickle module
-with open("index", mode="wb") as idx_file:
-    pickle.dump(idx, idx_file)
+    # persist the Index object with the pickle module
+    with open("index", mode="wb") as idx_file:
+        pickle.dump(idx, idx_file)
+
+except MemoryError as e:
+    print("MemoryError: Get more RAM, LUL!")
+    print(str(e))
+
+except Exception as e:
+    print(str(e))
